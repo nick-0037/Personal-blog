20ebe039a92f6c6d2758cbd076a10aa0bc5b98be
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2024-11-06 18:19:54 -0400

    Add admin routes and new views

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
+app.use(express.urlencoded({ urlencoded: true }))
+
 const pathArticles = path.join(__dirname, 'articles', 'articles.json')
 
 app.get('/', (req, res) => {
@@ -27,6 +29,85 @@ app.get('/article/:fileName', (req, res) => {
   } else {
     res.status(404).send('Article not found');
   }
+})
+
+// Admin section
+function generateFileName(title) {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+}
+
+app.get('/admin', (req, res) => {
+  const articlesData = fs.readFileSync(pathArticles, 'utf-8')
+  const articles = JSON.parse(articlesData)
+  
+  res.render('admin', { articles })
+})
+
+app.get('/admin/new', (req, res) => {
+  res.render('admin/new')
+})
+
+app.post('/admin/new', (req, res) => {
+  const articlesData = fs.readFileSync(pathArticles, 'utf-8')
+  const articles = JSON.parse(articlesData)
+
+  const fileName = generateFileName(req.body.title)
+
+  const newArticle = {
+    title: req.body.title,
+    date: new Date().toISOString().split('T')[0],
+    content: req.body.content,
+    fileName: fileName
+  }
+
+  articles.push(newArticle)
+
+  fs.writeFileSync(pathArticles, JSON.stringify(articles))
+  res.redirect('/admin')
+})
+
+app.get('/admin/edit/:fileName', (req, res) => {
+  const articlesData = fs.readFileSync(pathArticles, 'utf-8');
+  const articles = JSON.parse(articlesData);
+
+  const article = articles.find(a => a.fileName === req.params.fileName);
+  if (!article) {
+    return res.status(404).send('Article not found');
+  }
+
+  res.render('admin/edit', { article });
+});
+
+app.post('/admin/edit/:fileName', (req, res) => {
+  const articlesData = fs.readFileSync(pathArticles, 'utf-8');
+  const articles = JSON.parse(articlesData);
+  
+  const articleIndex = articles.findIndex(a => a.fileName === req.params.fileName)
+  if(articleIndex === -1) {
+    return res.statusCode(404).send('Article not found')
+  }
+
+  articles[articleIndex] = {
+    ...articles[articleIndex],
+    title: req.body.title,
+    date: req.body.date,
+    content: req.body.content
+  }
+
+  fs.writeFileSync(pathArticles, JSON.stringify(articles))
+  res.redirect('/admin')
+})
+
+app.post('/admin/delete/:fileName', (req, res) => {
+  const articlesData = fs.readFileSync(pathArticles, 'utf-8')
+  const  articles = JSON.parse(articlesData)
+
+  const updatedArticles = articles.filter(a => a.fileName !== req.params.fileName)
+  fs.writeFileSync(pathArticles, JSON.stringify(updatedArticles))
+  res.redirect('/admin')
 })
 
 const PORT = 3000
